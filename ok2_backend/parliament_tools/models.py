638fc8fa487b3c6d348366be86d8f25c80f23b7d
Moshe Nahmias <moshegrey@gmail.com>
2019-11-18 22:30:11 +0200

    add a few models

---
+++ ok2_backend/parliament_tools/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+from takanon.models import Takanon
+
+
+class ParliamentTools(models.Model):
+    description = models.TextField()
+    takanon = models.ForeignKey(Takanon, on_delete=models.CASCADE)
+    # TODO: how to do this? here/another table with more data? calculated or the view?
+    # next_date
