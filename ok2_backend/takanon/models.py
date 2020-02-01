e0c00276168f334be3c007c15982bdb5ca2aeae4
Igor Khanin <sadna@khanin.biz>
2020-02-01 23:59:42 +0200

    Ingest the Knesset rules of procedure into a model
    Read the latest version of the Knesset Rules of Procedure book
    ("Takanon") from the Hebrew WikiSource and save each clause as
    a model item. If a clause is already saved, it is updated.
    
    The clause text is a semi-structured JSON document capturing the
    breakdown into subclauses, sub-subclauses, etc.
    
    This pass does not track amendments to clauses.

--- ok2_backend/takanon/models.py
+++ ok2_backend/takanon/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 
 
-# Create your models here.
 class Section(models.Model):
-    name = models.CharField(unique=True)
-    ordinal = models.CharField()
+    ordinal = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.ordinal}: {self.name}"
 
 
 class Chapter(models.Model):
-    name = models.CharField()
-    ordinal = models.CharField()
+    ordinal = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=100)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.ordinal}: {self.name}"
 
 
 class Clause(models.Model):
-    name = models.CharField()
-    number = models.IntegerField(db_index=True, unique=True)
-    clause = models.TextField()
-    chapter = models.ForeignKey(Chapter, on_delete=models.SET_NULL)
-    # description = models.TextField()
+    name = models.CharField(max_length=200)
+    number = models.CharField(max_length=10, db_index=True, unique=True)
+    clause_text = models.TextField()
+    chapter = models.ForeignKey(Chapter, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.number}: {self.name}"
