7bb4d9cebeb882b1421379928976a9d1a61c7e0f
Moshe Nahmias <moshegrey@gmail.com>
2020-02-24 14:29:01 +0200

    tmp jwt

--- ok2_backend/ok2_backend/urls.py
+++ ok2_backend/ok2_backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from one_minute_speeches.views import check
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', check)
 ]
